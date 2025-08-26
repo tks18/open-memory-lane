e5707da01c88fe7bf79ff74513c820a00d6b1a1a
tks18 <tksudharshan@gmail.com>
2025-08-26 22:10:42 +0530

    fix(start_script): fix the start script to refer the correct start file

--- start_app.py
+++ start_app.py
@@ -1,25 +1,17 @@
 import subprocess
-import sys
 import os
-from pathlib import Path
-import yaml
 
-# Load the ocnfiguration from a YAML file if it exists
-with open(".config.yml", "r") as f:
-    cfg = yaml.safe_load(f)
-
-LOG_FOLDER = Path(cfg["paths"]["log_folder"])
+from app.helpers.config import LOG_FOLDER
 
 os.makedirs(LOG_FOLDER, exist_ok=True)  # Ensure the log folder exists
 
 LOG_PATH_APP_CLI = os.path.join(LOG_FOLDER, "recorder_cli.log")
-LOG_PATH_CLIENT = os.path.join(LOG_FOLDER, "client.log")
+LOG_PATH_CLIENT = os.path.join(LOG_FOLDER, "client_cli.log")
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 venv_python = os.path.join(base_dir, ".venv", "Scripts",
                            "python.exe")  # your venv python
 app_path = os.path.join(base_dir, "main.py")
-client_path = os.path.join(base_dir, "client.py")
 
 
 def run_hidden(script_path, log_path):
@@ -31,6 +23,5 @@ def run_hidden(script_path, log_path):
 
 
 run_hidden(app_path, LOG_PATH_APP_CLI)
-run_hidden(client_path, LOG_PATH_CLIENT)
 
-print("✅ Both scripts launched in background.")
+print("✅ Scripts launched in background.")
